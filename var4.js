323832c05b79e5a9f86522a9b43114ff0750a1d7
mehrinwagle <mehrinwagle26@gmail.com>
2025-10-15 16:15:07 +0530

    if else done

---
+++ var4.js
@@ -0,0 +1,12 @@
+//operators
+/* now we are
+learning operators*/
+
+let age = 18;
+let text = "you can drive";
+   
+if (age <= 18) {
+    text = "you cannot drive"
+}
+
+document.getElementById("demo").innerHTML=text;
---
+++ var5.js
@@ -0,0 +1,8 @@
+let a = 5;
+
+if (a%2 == 0){
+    a = "even";
+}
+else {a = "odd"}
+
+document.getElementById("demo2").innerHTML=a;

842c7dd68b4c3ca3eddfe492a09ec3f672ef47b4
mehrinwagle <mehrinwagle26@gmail.com>
2025-10-15 15:47:59 +0530

    datatypes

---
+++ var2.js
@@ -0,0 +1,13 @@
+var x = 5
+let y = 10
+const z = 15
+
+
+console.log(x,y,z)
+
+let i = 10;
+
+console.log(i)
+
+
+
---
+++ var3.js
@@ -0,0 +1,72 @@
+// Types of variables
+
+// String
+
+let x = "hello";
+
+console.log(x)
+console.log(typeof x)
+
+
+//Number
+
+let y = 100;
+let z = 1.25;
+
+console.log(y,z)
+console.log(typeof y,z)
+
+//Big int
+
+let s = 250000000000000n;
+let h = BigInt(10000000000000000);
+
+console.log(s,h)
+console.log(typeof s,h)
+
+//boolean
+
+let i = true;
+let d = false;
+
+console.log(i,d)
+console.log(typeof i)
+console.log(typeof d)
+
+//object 
+
+const var1 = {Name:"samiya",surname:"memon"};
+console.log(var1)
+console.log(typeof var1)
+
+//array object
+
+const cars = ['BMW','Ciaz','Audi'];
+console.log(cars)
+console.log(typeof cars)
+
+//date object
+
+const date = new Date(2025-10-15);
+console.log(date)
+console.log(typeof date)
+
+
+//undefined
+
+let p;
+console.log(p)
+console.log(typeof p)
+
+// Null
+
+let r = null;
+console.log(r)
+console.log(typeof r)
+
+
+//symbol
+
+const b = Symbol();
+console.log(b)
+console.log(typeof b)
